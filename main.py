4b97fa6e7141f6ac63965a1e2d664cb3d2e0d6d8
PorterOBrien2021 <824085@mystma.org>
2021-01-07 16:17:05 +0000

    command

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThen(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+def lessThen(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+def greaterOrEqual(x, y):
+    if x >= y:
+        return True
+    else:
+        return False
+def lessOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
+
